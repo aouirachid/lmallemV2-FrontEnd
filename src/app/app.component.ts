6b7dce6ff839660138b679e3e4ff203d6eda0a7d
Rachid AOui <aouirachid22@gmail.com>
2024-08-12 02:04:23 +0100

    in this commit we adjust the responsive mode 'the top-bar with the sidebar and toggeled function' and also update the dropdown in the user info by adding new packege 'ng-bootstrap'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NgClass],
+  imports: [RouterOutlet, RouterModule, NgClass,NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,24 +14,22 @@ export class AppComponent {
   title = 'lmallem-frontend';
   activeSubmenu: string = '';
   sidebarOpen = false;
+
+  //for submenu
   toggleSubmenu(menu: string): void {
     this.activeSubmenu = this.activeSubmenu === menu ? '' : menu;
   }
   
-  // @ViewChild('sidebar') sidebarElementRef: ElementRef;
-
+  //for sidebar
   toggleSidebar(): void {
     this.sidebarOpen = !this.sidebarOpen;
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar) {  // Checks if sidebar is not null
-        if (this.sidebarOpen) {
-            sidebar.classList.add('active');
-        } else {
-            sidebar.classList.remove('active');
-        }
-    } else {
-        console.error('Sidebar element not found!');
+    const sidebar = document.getElementById('sidebar') as HTMLElement;
+    const main = document.querySelector('main') as HTMLElement;
+    if (sidebar) {
+        sidebar.classList.toggle('active');
+        main.classList.toggle('active'); // Adjust main content too
     }
-}
+  }
 
+  
 }

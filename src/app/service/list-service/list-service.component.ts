6bfa462ecb47858bbf6803be86c871a095b4e8b8
rachidaoui <aouirachid22@gmail.com>
2025-03-07 02:53:42 +0000

    update inject method

--- src/app/service/list-service/list-service.component.ts
+++ src/app/service/list-service/list-service.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DataTablesModule } from 'angular-datatables';
 import { Config } from 'datatables.net';
@@ -11,20 +11,20 @@ import { ServiceService } from '../../services/service.service';
 @Component({
   selector: 'app-list-service',
   standalone: true,
-  imports: [RouterModule,NgFor,NgIf,DataTablesModule],
+  imports: [RouterModule, NgFor, NgIf, DataTablesModule],
   templateUrl: './list-service.component.html',
-  styleUrl: './list-service.component.css'
+  styleUrl: './list-service.component.css',
 })
-export class ListServiceComponent implements OnInit{
-  Service:any = [];
+export class ListServiceComponent implements OnInit {
+  Service: any = [];
   dtOptions: Config = {};
-  dttrigger:Subject<any> = new Subject<any>();
+  dttrigger: Subject<any> = new Subject<any>();
 
   constructor(
     private categoryService: CategoryService,
     private serviceService: ServiceService,
     private toastr: ToastrService
-  ){}
+  ) {}
 
   ngOnInit(): void {
     this.serviceService.getServices().subscribe(
@@ -38,20 +38,25 @@ export class ListServiceComponent implements OnInit{
       }
     );
     this.dtOptions = {
-      pagingType: 'full_numbers'
+      pagingType: 'full_numbers',
     };
   }
 
   delete(id: any, i: any) {
-    const confirmDelete = confirm("Are you sure you want to delete this Service? This action cannot be undone.");
+    const confirmDelete = confirm(
+      'Are you sure you want to delete this Service? This action cannot be undone.'
+    );
     if (confirmDelete) {
-      this.serviceService.deleteService(id).subscribe(() => {
-        this.Service.splice(i, 1);
-        this.toastr.error('Service deleted Successfully!', 'Deletion');
-      }, (err) => {
-        alert("An error occurred while trying to delete the Service.");
-        console.log(err);
-      });
+      this.serviceService.deleteService(id).subscribe(
+        () => {
+          this.Service.splice(i, 1);
+          this.toastr.error('Service deleted Successfully!', 'Deletion');
+        },
+        (err) => {
+          alert('An error occurred while trying to delete the Service.');
+          console.log(err);
+        }
+      );
     } else {
       this.toastr.error('Service deletion canceled', 'Deletion');
     }

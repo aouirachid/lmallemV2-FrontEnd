df6238d49a8b49d8f1379ffb668ea02aba5c3ddd
aouirachid <aouirachid22@gmail.com>
2025-10-10 02:20:13 +0100

    update prod url

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -26,25 +26,19 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          //console.error('401 Unauthorized detected in interceptor');
           this.authService.logout().subscribe({
             next: () => {
-              //console.log('Logged out successfully. Redirecting to login...');
               this.zone.run(() => {
                 this.router.navigate(['/auth/login']);
               });
             },
             error: (err) => {
-              //console.error('Error during logout:', err);
               this.zone.run(() => {
                 this.router.navigate(['/auth/login']);
               });
             },
           });
         } else if (error.status === 403) {
-          console.error(
-            '403 Forbidden detected. Redirecting to unauthorized...'
-          );
           this.router.navigate(['/unauthorized']);
         }
         return throwError(() => error);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-domain.com/api',
+  apiUrl: 'https://blmallem.aouis.com/public/api',
 };
